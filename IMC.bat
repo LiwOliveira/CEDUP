e58c6c5ad56331f05bb9f9f9c79faf3acac65461
Eliel <86987254+LiwOliveira@users.noreply.github.com>
2022-11-07 13:07:51 -0300

    Update IMC.bat

--- IMC.bat
+++ IMC.bat
@@ -1,18 +1,22 @@
 @echo off
 
-set autura = 0
-set idade = 0
+set altura = 0
 set peso = 0
+set sexo = 0
 set imc = 0
 
+:menu
+CLS
+set /p altura="Qual a sua altura? "
+set /p peso="Qual seu peso? "
+set /p sexo="Informe seu sexo, M ou F? " 
 
-teste
+imc = (%altura%*%altura%)/%peso%
 
-teste
+if %imc% equ 1 goto magro
+if %imc% equ 2 goto normal
+if %imc% equ 3 goto gordo
 
-
-
-5
-teste 2
-oi
-ola
+:magro
+set /p sexo="Magro"
+if %imc%
